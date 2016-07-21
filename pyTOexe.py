bb3adc7d597952b9cd9f7f3a678b91c0e41a0bf8
new-00-0ne <caleb.matthew.brown@gmail.com>
2016-07-21 15:29:55 -0400

    Delete pyTOexe.py

--- pyTOexe.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-if len(sys.argv) > 1:
-	None
-else:
-	file = input("Which script do you want to compile?:")
-	os.system("C:\Python34\Scripts\cxfreeze "+file)
-	print("done!")
